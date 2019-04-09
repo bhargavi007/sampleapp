975f7b70f9635d4282c924d4223e34d18770fcf2
Bhargavi <bhargavi.addagulla@zomato.com>
2019-04-10 01:35:57 +0530

    changed migration file

--- db/migrate/20190409192356_initial_migration.rb
+++ db/migrate/20190409192356_initial_migration.rb
@@ -1,9 +1,14 @@
-class CreateBooks < ActiveRecord::Migration
+class InitialMigration < ActiveRecord::Migration
   def change
     create_table :books do |t|
       t.string :name, null:false
       t.string :author_id, null:false
       t.timestamps :created_at, null: false, :default => Time.now
+    end
+
+    create_table :authors do |t|
+      t.string :name, null:false
+      t.timestamps null: false
     end
   end
 end
